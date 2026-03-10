f0419c964bebd9a94bc12d22dfd5d25bc4ed7de2
Alex Lewis <150391000+alexlewis9@users.noreply.github.com>
2026-03-10 14:21:50 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${lato.variable} ${lato.variable}`}>
         <Providers>
           {children}
           <Footer />
